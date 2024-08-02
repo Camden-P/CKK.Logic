7e0b2062e31dc65df48ceed6b4f7333fed48d1cc
CAMDEN PRATT <CAMDEN.PRATT1674@OMAIL.OTECH.EDU>
2024-08-02 08:25:48 -0600

    Added classes

---
+++ CKK.Logic/CKK.Logic/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    internal class Customer
+    {
+        // Instance Variables
+        private int _id;
+        private string _name;
+        private string _address;
+
+        // Methods
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string GetAddress()
+        {
+            return _address;
+        }
+        public void SetAddress(string address)
+        {
+            _address = address;
+        }
+    }
+}
