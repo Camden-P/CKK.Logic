e7bcfa0654d8008b606c0d1ebb8065eef3aea746
CAMDEN PRATT <CAMDEN.PRATT1674@OMAIL.OTECH.EDU>
2024-09-16 14:57:06 -0600

    Added interfaces.

---
+++ CKK.Logic/CKK.Logic/Interfaces/IShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using CKK.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Interfaces
+{
+    internal interface IShoppingCart
+    {
+        int GetCustomerId();
+        ShoppingCartItem AddProduct(Product product, int quantity);
+        ShoppingCartItem RemoveProduct(int id, int quantity);
+        decimal GetTotal();
+        ShoppingCartItem GetProductById(int id);
+        List<ShoppingCartItem> GetProducts();
+    }
+}
